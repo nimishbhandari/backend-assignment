8f8c9d699d2306952f178f0bb5b8646e7284e139
Nimish Bhandari <nimishbhandari727521@gmail.com>
2022-02-09 11:11:03 +0530

    basic setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT;
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(
+    `Greetings From <a href="https://bit.ly/bnimish"> Nimish Bhandari </a> &hearts;`
+  );
+});
+
+const PORT = process.env.PORT || 5000;
 const MODE = process.env.MODE;
 app.listen(PORT, () => {
-  console.log(`The server is running on ${PORT} in ${MODE} mode`);
+  console.log(`Server Running on PORT:${PORT} in ${MODE} mode`);
 });
